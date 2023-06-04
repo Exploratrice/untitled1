f03fd325e16e2581d7a209670ae48535c75c3d79
Explora <exploratrice2019@mail.ru>
2023-06-04 21:12:20 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13676; // Стоимость билета
+        int mile = 20; // Стоимость одной мили
+
+        int result = price / 20;
+
+        System.out.println("Количество начисленных миль: ");
+        System.out.println((result) + " бонусных(е) миль(и)");
+    }
+}
